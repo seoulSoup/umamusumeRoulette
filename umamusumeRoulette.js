15939674c970d9c3bf27ec41d660c8050a9cba08
seoulSoup <ragnarok3rd@gmail.com>
2023-02-10 13:08:31 +0900

    modified:   umamusumeRoulette.css 	modified:   umamusumeRoulette.js

--- umamusumeRoulette.js
+++ umamusumeRoulette.js
@@ -16,33 +16,38 @@ const consoleToHtml = function() {
 }
 window.console.log = consoleToHtml
 
-
-let DictRange = {0: ["단거리", "#E0BBE4"],	// 단거리: 오이 제외
-				1: ["마일", "#957DAD"],		// 마일: 오이 제외
-				2: ["중거리", "#D291BC"],	// 중거리: 오이 제외
-				3: ["장거리", "#FEC8D8"],			// 장거리: 니이가타, 츄쿄, 오이 제외
-				4: ["더트", "#FFDFD3"], 		// 더트: 삿포로, 코쿠라 제외
-				};
-let DictLoc = {0: ["삿포로", "#C7E3A4"],
-				1: ["하코다테", "#FDF6C3"],
-				2: ["후쿠시마", "#FFFEF7"],
-				3: ["니이가타", "#F8CBEE"],
-				4: ["도쿄", "#CAC3F7"],
-				5: ["나카야마", "#C3E9F8"],
-				6: ["츄쿄", "#F075AB"],
-				7: ["교토", "#F2BD93"],
-				8: ["한신", "#FAD8CC"],
-				9: ["코쿠라", "#F5A6CD"],
-				10: ["오이", "#D879C9"],
-				11: [`url("https://github.com/seoulSoup/umamusumeRoulette/blob/main/assets/icon(squre)/big.png?raw=true)`, "#CAC3F7"]
-				};
-let DictRot = {0: ["시계 (우)", "#F595B2"], 1: ["반시계 (좌)", "#FFB3B3"], 2: ["직선", "#FFF1BA"]};
-let DictSide = {0: ["내주로", "#BEE3ED"], 1: ["외주로", "#AFADDE"], 2: ["외->내", "#FFA694"], 3: ["없음", "#BFEDBE"]};
-let DictLength = {0: ["1000m", "#E0BBE4"], 1: ["1150m", "#957DAD"], 2: ["1200m", "#D291BC"], 3: ["1300m", "#FEC8D8"], 4: ["1400m", "#FFDFD3"], 5: ["1500m", "#C7E3A4"],
-				 6: ["1600m", "#FDF6C3"], 7: ["1700m", "#FFFEF7"], 8: ["1800m", "#F8CBEE"], 9: ["1900m", "#CAC3F7"], 10: ["2000m", "#C3E9F8"], 11: ["2100m", "#F075AB"],
-				 12: ["2200m", "#F2BD93"], 13: ["2300m", "#FAD8CC"], 14: ["2400m", "#F5A6CD"], 15: ["2500m", "#D879C9"], 16: ["2600m", "#F595B2"], 17: ["3000m", "#FFB3B3"],
-				 18: ["3200m", "#BADB9E"], 19: ["3400m", "#FDEEB4"], 20: ["3600m", "#FAADE3"]
-				}
+					// index0: 거리
+let DictTotalInfo = {0: {0: ["단거리", "#E0BBE4"],	// 단거리: 오이 제외
+						1: ["마일", "#957DAD"],		// 마일: 오이 제외
+						2: ["중거리", "#D291BC"],	// 중거리: 오이 제외
+						3: ["장거리", "#FEC8D8"],			// 장거리: 니이가타, 츄쿄, 오이 제외
+						4: ["더트", "#FFDFD3"], 		// 더트: 삿포로, 코쿠라 제외
+						},
+						// index1: 경기장
+					1: {0: ["삿포로", "#C7E3A4"],
+						1: ["하코다테", "#FDF6C3"],
+						2: ["후쿠시마", "#FFFEF7"],
+						3: ["니이가타", "#F8CBEE"],
+						4: ["도쿄", "#CAC3F7"],
+						5: ["나카야마", "#C3E9F8"],
+						6: ["츄쿄", "#F075AB"],
+						7: ["교토", "#F2BD93"],
+						8: ["한신", "#FAD8CC"],
+						9: ["코쿠라", "#F5A6CD"],
+						10: ["오이", "#D879C9"],
+						11: [`url("https://github.com/seoulSoup/umamusumeRoulette/blob/main/assets/icon(squre)/big.png?raw=true)`, "#CAC3F7"]
+						},
+						// index2: 방향
+					2: {0: ["시계 (우)", "#F595B2"], 1: ["반시계 (좌)", "#FFB3B3"], 2: ["직선", "#FFF1BA"]},
+						// index3: 내/외
+					3: {0: ["내주로", "#BEE3ED"], 1: ["외주로", "#AFADDE"], 2: ["외->내", "#FFA694"], 3: ["없음", "#BFEDBE"]},
+						// index4: 세부 거리
+					4: {0: ["1000m", "#E0BBE4"], 1: ["1150m", "#957DAD"], 2: ["1200m", "#D291BC"], 3: ["1300m", "#FEC8D8"], 4: ["1400m", "#FFDFD3"], 5: ["1500m", "#C7E3A4"],
+						6: ["1600m", "#FDF6C3"], 7: ["1700m", "#FFFEF7"], 8: ["1800m", "#F8CBEE"], 9: ["1900m", "#CAC3F7"], 10: ["2000m", "#C3E9F8"], 11: ["2100m", "#F075AB"],
+						12: ["2200m", "#F2BD93"], 13: ["2300m", "#FAD8CC"], 14: ["2400m", "#F5A6CD"], 15: ["2500m", "#D879C9"], 16: ["2600m", "#F595B2"], 17: ["3000m", "#FFB3B3"],
+						18: ["3200m", "#BADB9E"], 19: ["3400m", "#FDEEB4"], 20: ["3600m", "#FAADE3"]
+						}
+}
 
 let DictLUT = {0: {0: {0: {3: [2]}},	// 삿포로
 				1: {0: {3: [2, 0]}}, 	// 하코다테
@@ -89,7 +94,7 @@ let DictLUT = {0: {0: {0: {3: [2]}},	// 삿포로
                     1: {0: {3: [16]}}, 	// 하코다테
                     2: {0: {3: [16]}},	// 후쿠시마
                     4: {1: {3: [19, 15]}},	// 도쿄
-                    5: {0: {0: [1920, 15]}},	// 나카야마
+                    5: {0: {0: [20, 15]}},	// 나카야마
                     7: {0: {3: [18, 17]}},	// 교토
                     8: {0: {0: [17],
                             1: [16],
@@ -124,6 +129,7 @@ function createSlots (ring, DictTemp) {
 	for (var i = 0; i < SLOTS_PER_REEL; i ++) {
 		var slot = document.createElement('div');
 		slot.className = 'slot';
+		slot.id = 'ring' + (ListCurrentRings.length+1) + 'slot' + i;
 		var genNext = genTemp.next().value;
 		// compute and assign the transform for this slot
 		var transform = 'rotateX(' + (slotAngle * i) + 'deg) translateZ(' + REEL_RADIUS + 'px)';
@@ -139,7 +145,6 @@ function createSlots (ring, DictTemp) {
 					// .css("object-position", "50% 50%")
 		}
 		else {
-			
 			$(slot).append(content);
 		}
 		ListSlot.push(genNext);
@@ -190,64 +195,83 @@ function spin(timer) {
 	DictRoute[COUNT++] = ListLUT;
 	console.log(DictRoute);
 	for(var i = 1; i < 6; i ++) {
-		console.log($('#ring'+i).attr('class'));
+		// console.log($('#ring'+i).attr('class'));
 		
 		let ListCurrentRing = ListCurrentRings[i-1];
 		var answer = ListLUT[i-1];
 		// Condition
-		var oldClass = $('#ring'+i).attr('class');
-		if(oldClass.length > 4) {
-			oldSeed = parseInt(oldClass.slice(10));
-		}
+		var oldClass = $('#ring'+i).attr('class').split("-");
+		var oldSeed = Number(oldClass[1]);
+		// Check Slot Class is orig or rev
+		var flagOldSeed = true;
+		if(oldClass.length > 2) flagOldSeed = false;
 		// Checking Answer in Ring?
+		// k: randomized list index of answer
 		let ListAnswerIdx = [];
 		for (var k = 0; k < ListCurrentRing.length; k++){
-			if (answer == ListCurrentRing[k][0] && (k + 10)%12 != oldSeed) ListAnswerIdx.push(k);
+			if (answer == ListCurrentRing[k][0]) ListAnswerIdx.push(k);
 		}
 		// Case: Answer is in Ring already
-		if (ListAnswerIdx.length > 0){
-			var seed = getSeed(ListAnswerIdx.length);
-			while(oldSeed == ListAnswerIdx[seed]) {
-				seed = getSeed(ListAnswerIdx.length);
+		if (ListAnswerIdx.length > 1 ) {
+			var seed = (ListAnswerIdx[getSeed(ListAnswerIdx.length)] + 10 ) % 12;
+			while (seed == oldSeed){
+				seed = (ListAnswerIdx[getSeed(ListAnswerIdx.length)] + 10 ) % 12;
 			}
-			console.log("oldSeed: ", oldSeed); // index of answer in ring
-			console.log("seed: ", (ListAnswerIdx[seed] + 10) % 12); // index of answer in ring
-			console.log(ListCurrentRing[ListAnswerIdx[seed]]); // answer
-			seed = (ListAnswerIdx[seed] + 10) % 12;
-		} 
+		}
+		else if (ListAnswerIdx.length == 1){
+			var seed = (ListAnswerIdx[0] + 10 ) % 12;
+			// var strSeed = seed;
+			// if (flagOldSeed) strSeed = seed + '-' + 2;
+		}
 		// Case: Answer is not in Ring => Ju-jack
 		else {
-			seed = 5;
+			seed = (oldSeed + 5) % 12;
 			// var slot = $('#ring'+i).slot;
-			console.log("changed");
-			console.log($('#ring'+i).children("3").attr("class"));
-			// $('#ring'+i).children("3").css("background-image", `url("https://github.com/seoulSoup/umamusumeRoulette/blob/main/assets/icon(squre)/big.png?raw=true)`);
-			// slot.className = 'slot';
-			// var genNext = genTemp.next().value;
-			// // compute and assign the transform for this slot
-			// slot.style.backgroundColor = genNext[1][1];
-			// // setup the number to show inside the slots
-			// var content = genNext[1][0]
-			// if (content.includes("url")){
+			$('#ring'+i+'slot' + (oldSeed + 7) % 12).text(DictTotalInfo[i-1][answer][0]);
+			ListCurrentRings[i-1][(oldSeed + 7) % 12] = [answer, DictTotalInfo[i-1][answer]];
+			// $('#ring'+i+'slot5').text() = "0";
+			// console.log($('#ring'+i+'slot5').text);
+			// var seed = getSeed(ListAnswerIdx.length);
+
+			// $('#ring'+i+'slot5').css("background-image", `url("https://github.com/seoulSoup/umamusumeRoulette/blob/main/assets/icon(squre)/big.png?raw=true)`);
 			// 	$(slot).css("background-image", content)
 			// 			.css("background-size", "contain")
 			// 			.css("background-position", "center")
 			// 			.css("background-repeat", "no-repeat")
 			// }
+			// console.log(ListCurrentRings[i-1]); // answer
+			
 		}
+		// var spinSeed = seed;
+		if (seed == oldSeed){
+			// console.log("start");
+			$('#ring'+i)
+			.css('animation','back-spin 1s, spin-' + seed + ' ' + (timer + i*0.5) + 's')
+			// console.log("end");
+		// 	console.log(oldClass);
+		// 	console.log("Seed: ", seed);
+		// 	console.log("oldSeed: ", oldSeed);
+		// 	// if (flagOldSeed) spinSeed = seed + '-' + 2;
+		// 	// spinSeed = seed + '-' + 2;
+		// 	// spinSeed = 0;
+		// 	console.log(spinSeed);
+		}
+		else {
+			// Animation
+			$('#ring'+i)
+			.css('animation','back-spin 1s, spin-' + seed + ' ' + (timer + i*0.5) + 's')
+			.attr('class','ring spin-' + seed);  // Last State (Result)
+		}
+		
+
 		// Current Win position Index: oldSeed + 2
 		// We have to put answer into this position
-		
 		// seed = (12 + ListAnswerIdx[seed])%12;
-		// var seed = getSeed(SLOTS_PER_REEL);
-		// while(oldSeed == seed) {
-		// 	seed = getSeed(SLOTS_PER_REEL);
-		// }
 
-		// Current index - seed + 2 = Win index
-		$('#ring'+i)
-			.css('animation','back-spin 1s, spin-' + seed + ' ' + (timer + i*0.5) + 's')
-			.attr('class','ring spin-' + seed);
+		// Animation
+		// $('#ring'+i)
+		// 	.css('animation','back-spin 1s, spin-' + seed + ' ' + (timer + i*0.5) + 's')
+		// 	.attr('class','ring spin-' + seed);  // Last State (Result)
 	}
 	// console.log(DictRoute);
 	// console.log('=====');
@@ -261,11 +285,11 @@ $(document).ready(function() {
 	// ring3:spin3 (-150deg + back60deg : idx - 3) Win: ListSlot3[5]
 	// ring4:spin1 (-90deg + back60deg : idx - 1) Win: ListSlot4[3]
 	// ring5:spin2 (-120deg + back60deg : idx - 2) Win: ListSlot5[4]
-	createSlots(document.querySelector('#ring1'), DictRange);
-	createSlots(document.querySelector('#ring2'), DictLoc);
-	createSlots(document.querySelector('#ring3'), DictRot);
-	createSlots(document.querySelector('#ring4'), DictSide);
-	createSlots(document.querySelector('#ring5'), DictLength);
+	createSlots(document.querySelector('#ring1'), DictTotalInfo["0"]);
+	createSlots(document.querySelector('#ring2'), DictTotalInfo["1"]);
+	createSlots(document.querySelector('#ring3'), DictTotalInfo["2"]);
+	createSlots(document.querySelector('#ring4'), DictTotalInfo["3"]);
+	createSlots(document.querySelector('#ring5'), DictTotalInfo["4"]);
 
 	// Button Start
 	$(".go").click(function(){
